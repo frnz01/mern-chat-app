1d0d8ddfd45eb0472d9882067cbf8021b63c8bd5
franz <franz.dulnuan9@gmail.com>
2024-08-22 20:34:36 +0800

    fixed deployment issues

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,43 @@
-import express from "express";
+// import express from "express";
+// import dotenv from "dotenv";
+// import cookieParser from "cookie-parser";
+// import path from "path";
+
+// import authRoutes from "./routes/auth.route.js";
+// import messageRoutes from "./routes/message.route.js";
+// import userRoutes from "./routes/user.route.js";
+
+// import connectToMongoDB from "./db/connectToMongoDb.js";
+// import { app, server } from "./socket/socket.js";
+
+// const PORT = process.env.PORT || 5000;
+
+// const __dirname = path.resolve();
+
+// dotenv.config();
+
+// app.use(express.json());
+// app.use(cookieParser());
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/messages", messageRoutes);
+// app.use("/api/users", userRoutes);
+
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
+
+// server.listen(PORT, () => {
+//   connectToMongoDB();
+//   console.log(`Server Running on port ${PORT}`);
+// });
+
 import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
 
@@ -12,9 +50,8 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+// Define __dirname for ES modules
 const __dirname = path.resolve();
-
-dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -31,5 +68,5 @@ app.get("*", (req, res) => {
 
 server.listen(PORT, () => {
   connectToMongoDB();
-  console.log(`Server Running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
